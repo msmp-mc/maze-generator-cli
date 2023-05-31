00e6e754779013b44e33987a920a44f626ed64ad
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-05-31 15:28:02 +0200

    feat(maze): various getter for Maze

--- Generators/global.go
+++ Generators/global.go
@@ -24,15 +24,30 @@ func GenerateNewMaze(w uint, h uint, algo func(*Maze) error) (Maze, error) {
 
 // generateWalls generate the default walls
 func (m *Maze) generateWalls() []Wall {
-	xV := (m.Width - 1) * m.Height
-	xH := m.Width * m.Height
-	x := xH + xV
+	xH := m.GetHorizontalWallsNumber()
+	x := xH + m.GetVerticalWallsNumber()
 	walls := make([]Wall, x)
 	for i := uint(0); i < x; i++ {
 		walls[i] = Wall{IsVertical: i >= xH, ID: int(i)}
 	}
 	m.Walls = walls
 	return walls
+}
+
+func (m *Maze) GetHorizontalWallsNumber() uint {
+	return m.Width * m.Height
+}
+
+func (m *Maze) GetVerticalWallsNumber() uint {
+	return (m.Width - 1) * m.Height
+}
+
+func (m *Maze) GetVerticalWalls() []Wall {
+	return m.Walls[m.GetHorizontalWallsNumber():]
+}
+
+func (m *Maze) GetHorizontalWalls() []Wall {
+	return m.Walls[:m.GetHorizontalWallsNumber()-1]
 }
 
 func (m *Maze) RenderWalls() {
--- Generators/randomised_kruskal_test.go
+++ Generators/randomised_kruskal_test.go
@@ -7,13 +7,29 @@ import (
 )
 
 func TestWallsRandomisedKruskal(t *testing.T) {
-	maze, err := GenerateNewMaze(5, 5, NewRandomisedKruskal)
+	m, err := GenerateNewMaze(5, 5, NewRandomisedKruskal)
 	if err != nil {
 		t.Error(err.Error())
 		return
 	}
-	if len(maze.Walls) != 45 {
+	if len(m.Walls) != 45 {
 		t.Error(utils.FormatTestError("bad length of total walls", strconv.Itoa(45),
-			strconv.Itoa(len(maze.Walls))))
+			strconv.Itoa(len(m.Walls))))
+	}
+
+	vertical := m.GetVerticalWalls()
+	for _, wall := range vertical {
+		if !wall.IsVertical {
+			t.Error(utils.FormatTestError("non vertical walls in GetVerticalWalls", "true", "false"))
+			break
+		}
+	}
+
+	horizontal := m.GetHorizontalWalls()
+	for _, wall := range horizontal {
+		if wall.IsVertical {
+			t.Error(utils.FormatTestError("vertical walls in GetHorizontalWalls", "false", "true"))
+			break
+		}
 	}
 }
